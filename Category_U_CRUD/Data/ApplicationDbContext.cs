ba679ceb2ccd28f0f2f59f913ab57388834d328c
Ro-hit96 <rohitsurvase4363@gmail.com>
2024-03-02 17:26:16 +0530

    Add project files.

---
+++ Category_U_CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Category_U_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Category_U_CRUD.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1,Name="Mobile",DisplayOrder=12},
+                new Category { Id = 3, Name = "Laptop", DisplayOrder = 12 }
+                );
+        }
+
+    }
+}
---
+++ Category_U_CRUD/Migrations/20240302111234_SeedCategoryTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Category_U_CRUD.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 12, "Mobile" },
+                    { 3, 12, "Laptop" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
